bbc1dfed6c96cc8f87ca217694378ac52340bb82
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-05-20 01:16:56 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.API_PORT || 3000;
+  app.enableCors();
   await app.listen(port);
   console.log(`server is running on http://localhost:${port}`);
 }
